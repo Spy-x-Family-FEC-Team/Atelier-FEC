b5d7eb3396f8d02b4ceeee77ac016f980e6e1cce
Markus Leben <markus_leben@mac.com>
2023-03-17 19:47:13 -0700

    Installed fontawsome and got the star box working

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { render } from "react-dom";
 import Reviews from "./components/Reviews";
+import StarRating from "./components/assets/StarRating.jsx";
 
 const FoldWrapper = styled.section`
 margin: auto;
@@ -22,9 +23,9 @@ const App = () => (
   <FoldWrapper>
     <div>Hello u stinky World</div>
     <div>Overview would go here in the slightly thicker bit</div>
-
   </FoldWrapper>
   <BelowFoldWrapper>
+    <StarRating rawRating={3.7}/>
   {/* <Overview/>
 <Related/>
 <QnA/> */}
