2e82467feaec6581ab0e75fa45e37102dfeda1ba
Markus Leben <markus_leben@mac.com>
2023-03-20 11:05:13 -0700

    split docs comments into individual files

--- client/src/components/Reviews/Ratings/index.jsx
+++ client/src/components/Reviews/Ratings/index.jsx
@@ -22,3 +22,31 @@ const Ratings = () => (
 );
 
 export default Ratings;
+
+/*
+Rating Breakdown (Filtering)
+A breakdown of the ratings received for the product will double as the filtering options for the reviews list. This breakdown will display at the top left corner of the Rating and Reviews module.
+1.2.4.1. Rating summary
+The top of the breakdown will simply display the average rating for the product. The rating should be displayed both as a number value as well as represented by star icons.
+The number displayed should be rounded to the nearest single decimal.
+The average rating of the product will be displayed in the format of solid or outlined stars, where the solid stars represent the review score. A total of 5 stars should always appear, and the amount filled in should correspond to the average score.
+The visual for rating should be representative of up to a quarter of a review point. For example, if the average is 3.8, this should display as 3¾ solid stars and 1¼ outlined stars.
+Additionally, the count of total reviews should be listed.
+1.2.4.2. Breakdown
+Below the average rating, a section titled “Rating Breakdown” will appear. Within this section, the rating distribution will be broken down.
+5 bars will appear; one for each of the star ratings that a customer can give to a product. To the left of the bar, the star count will be explicitly stated in the form “# Stars”.
+To the right of the bar, a label with the total number of reviews submitted with that particular rating will be shown. Only the count will show.
+The bar itself will be two toned, green and gray. The portion of the bar which is green will represent the percentage of total reviews which have been submitted that have that particular star rating. For example, if a product has received 200 reviews and 150 are 5 stars, then the “5 stars” bar should be three quarters green.
+The green portion of the bar should always be left of the gray portion.
+If all the reviews submitted for a product are the same rating, then the bar for that star rating will be completely green and the other bars will be completely gray.
+Each rating breakdown, consisting of the label, the bar, and the count, will act as a filter. Clicking on the breakdown for a star count will filter the reviews list so that only reviews of that rating display. Hovering over this breakdown will change the background color to indicate it is actionable.
+The filters will be additive. If the list has already been filtered for 1 star reviews, clicking on the “2 star” breakdown will update the reviews list such that 1 and 2 star reviews are displayed.
+The filters will be toggled on and off with each click. Clicking a second time on a rating breakdown will remove the filter for that rating type. If this action removes the last or only filter, then the list should return to its default state and all reviews should be shown.
+Once one or more filters have been applied, a message will appear below the “Rating Breakdown” title. It will state the filters that have been currently applied. It will also include a link to “Remove all filters”. Clicking on this link will remove any filters applied and return the list to the default state.
+1.2.4.3. Recommendations
+The percentage of reviews that ‘recommend’ the product will be displayed below the breakdown.
+Product Breakdown (Factors)
+Reviews will provide the ability to give feedback on specific characteristics of the product. The characteristics include Size, Width, Comfort, Quality, Length, and Fit. One or more of these may be relevant for a product. In the Reviews module, the average feedback received will be displayed for all characteristics which apply to the product.
+Feedback for characteristics will be on a 5 point scale. The range of this scale will depend on the characteristic in question. For example, Size can range from (1) “too small” to (5) “too big”, with the middle option (3) being “perfect”. Using the same 5 point scale for Quality, however, the scale would range from (1) “poor” to (5) “great”.
+Regardless of what the range of the scale represents, the 5 point scale will display the same for all of the characteristics of the product. Each will appear as a gray bar similar to the rating breakdown. Above the bar, a label will state the characteristic. Below the bar, the meaning of the lowest selection (1) and the highest selection (5) will appear. On the bar, a single icon will appear representing the average value received via reviews submitted. The icon should appear horizontally from the left edge of the bar such that it represents the average input for the characteristic. For example, if the average is 5, the icon should appear all the way to the right. An average of 3 should appear in the middle.
+*/
---
+++ client/src/components/Reviews/ReviewList/ReviewListItem.jsx
@@ -0,0 +1,29 @@
+
+/* Individual Review Tile
+Each review will be displayed on a single tile within the list. The tile will display the following information:
+Star Rating - This will be the rating given to the product by this individual review.. The rating will be displayed in the format of solid or outlined stars, where the solid stars represent the review score. A total of 5 stars should always appear, and the amount filled in should correspond to the average score.
+The visual for rating should be representative of up to a quarter of a review point. For example, if the average is 3.8, this should display as 3¾ solid stars and 1¼ outlined stars.
+Date of review - The date the review was written should appear in the format “Month DD, YYYY”
+Review Summary - Reviews submitted will have a one sentence summary. This single sentence will be capped at 60 characters. On the review tile, this summary will appear in bold font above the full review.
+Review Body - The review body will be a free-form multimedia input where the user can submit text and images regarding their experience with the product.
+The text submitted as part of the review will be between 50 and 1000 characters long.
+Users should be able to submit up to 5 images along with a single review.
+By default the first 250 characters of the review should display. If the review is longer than 250 characters, below the body a link reading “Show more” will appear. Upon clicking this link, the review tile should expand and the rest of the review should display.
+Any images that were submitted as part of the review should appear as thumbnails below the review text. Upon clicking a thumbnail, the image should open in a modal window, displaying at full resolution. The only functionality available within this modal should be the ability to close the window.
+Recommend - If the reviewer recommends buying the product, the text “I recommend this product” and a checkmark icon will display below the review. If the reviewer does not recommend the product, nothing will display here.
+Reviewer name - The username for the reviewer will appear. Only the username will appear. No email addresses or other personal information will display. However, if the user’s email is associated with a sale in the system then next to the username the text “Verified Purchaser” will appear.
+Response to Review - Our internal sales team has the ability to respond to any reviews written. If the review has a corresponding response, this should appear below the reviewer name. The response should be preceded by the text “Response from seller”, and should be visually distinguished from the rest of the review.
+Rating Helpfulness - Any user on the site will have the ability to provide feedback on whether reviews are helpful. At the bottom of the review the text “Was this review helpful?” will precede two links “Yes (#)” and “No (#)”. Following “Yes” and “No” will be the count of users that have selected that button. Clicking either link should cast a vote for that selection.
+A user on the site does not need to be logged in to provide feedback on helpfulness.
+A user can provide feedback on any review. However, they can only make one submission for each review. If the user selects either “Yes” or “No” for a review, they should not be able to select another option again for that review.
+Sort Options
+Users will be able to change this ordering. A dropdown labeled “Sort on'' will contain options on sort order.
+The options within the sort drop down should include
+Helpful - This sort order will prioritize reviews that have been found helpful. The order can be found by subtracting “No” responses from “Yes” responses and sorting such that the highest score appears at the top.
+Newest - This is a straightforward sort based on the date the review was submitted. The most recent reviews should appear first.
+Relevant - Relevance will be determined by a combination of both the date that the review was submitted as well as ‘helpfulness’ feedback received. This combination should weigh the two characteristics such that recent reviews appear near the top, but do not outweigh reviews that have been found helpful. Similarly, reviews that have been helpful should appear near the top, but should yield to more recent reviews if they are older.
+By default, the reviews in the list should appear in order of relevance. The dropdown should display “Relevant” as the currently selected option.
+Upon selecting any of the other options, the dropdown should update to display the current selection, and the list should refresh to show the first results for that sort order.
+Changing the sort order will always refresh the reviews list.
+The sort selected should persist even when filters are added or removed.
+*/
