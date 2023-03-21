9ae87bf98427306b41edd45642bf593746069459
Jenessa Peterson <jenessa.peterson@gmail.com>
2023-03-21 13:40:02 -0700

    Vertical Carousel section renders over the main image, but there are no images in this yet.

---
+++ client/src/components/Overview/VerticalCarousel.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+// const StyledVerticalCarousel = styled.section`
+
+// `;
+
+// const StyledMainImage = styled.img`
+//   max-height: 70vh;
+//   max-width: 100%;
+//   z-axis:1;
+// `;
+
+// const StyledVerticalCarouselWrapper = styled.section`
+//   z-axis:2;
+//   margin-left:20px;
+//   margin-top:20px;
+// `;
+
+
+const VerticalCarousel = (props) => {
+
+  return(
+    <div>
+      Vertical Carousel will go here.
+    </div>
+  )
+};
+
+export default VerticalCarousel;
