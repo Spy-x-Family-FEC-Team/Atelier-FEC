b59e6326de238a9ab3cc8fe8c4178cae5ad9807b
Markus Leben <markus_leben@mac.com>
2023-03-28 14:22:22 -0700

    Revised Jest testing to mirror current data.

--- client/src/components/__tests__/Reviews.test.js
+++ client/src/components/__tests__/Reviews.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import Reviews from '/client/src/components/Reviews/index.jsx';
+import product from '/server/exampleData/product.json'
 import reviews from '/server/exampleData/reviews.json';
 import reviewData from '/server/exampleData/reviewData.json';
 
@@ -10,9 +11,9 @@ afterEach(() => {
 })
 
 test("should correctly render Reviews given dummy data", () => {
-  const { getByText, getByLabelText } = render(<Reviews reviewData={reviewData} reviews={reviews}/>);
+  const { getByText, getByLabelText } = render(<Reviews reviewData={reviewData} reviews={reviews} product={product}/>);
 
   screen.debug();
 
-  getByText('Hello from the Write a Review modal window!');
+  getByText('Write Your Own Review');
 })
